7246c6146238f10bbbc464d19c12db0075b67edf
Thomas Holmes <thomasholmes@outlook.com>
2023-01-29 18:45:26 +0000

    Added basic car and arena.

---
+++ combine.gd
@@ -0,0 +1,15 @@
+extends VehicleBody
+
+var max_rpm = 500
+var max_torque = 200
+
+func _physics_process(delta):
+	steering = lerp(steering, Input.get_axis("right","left") * 0.4, 5 * delta)
+	var acceleration = Input.get_axis("back","forward")
+	
+	var left_rpm = $back_left_wheel.get_rpm()
+	$back_left_wheel.engine_force = acceleration * max_torque * (1 - left_rpm / max_rpm)
+	
+	var right_rpm = $back_right_wheel.get_rpm()
+	$back_right_wheel.engine_force = acceleration * max_torque * (1 - right_rpm / max_rpm)
+	
