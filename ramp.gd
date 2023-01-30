58b75e4d7858eb0cb2571fb1743adbba442707ff
Thomas Holmes <thomasholmes@outlook.com>
2023-01-30 21:53:31 +0000

    Added a ramp.

---
+++ ramp.gd
@@ -0,0 +1,20 @@
+tool
+
+extends Spatial
+
+
+export (int, 1, 50) var length = 1
+export (int, 1, 50) var height = 1
+export (int, 1, 50) var width = 1
+
+func _enter_tree():
+	$MeshInstance.scale.x = length
+	$MeshInstance.scale.y = height
+	$MeshInstance.scale.z = width
+	pass
+
+func _process(delta):
+	if Engine.editor_hint:
+		$MeshInstance.scale.x = length
+		$MeshInstance.scale.y = height
+		$MeshInstance.scale.z = width
