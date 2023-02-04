ee5def0dee603e1ad93bf8d3c8b9f52553053cc2
Thomas Holmes <thomasholmes@outlook.com>
2023-02-04 23:30:46 +0000

    Added engine sound loop.

--- readme.md
+++ readme.md
@@ -8,3 +8,4 @@ Fira Sans font: https://fonts.google.com/specimen/Fira+Sans
 Wheat image: https://www.freepik.com/free-vector/realistic-wheat-composition_6128967.htm
 Side hay bale texture: https://create.vista.com/unlimited/stock-photos/220985178/stock-photo-hay-bale-isolated-white-background/
 Hay sound sample: https://www.youtube.com/watch?v=lMlY5yFZ-b8
+Engine sound sample: https://opengameart.org/content/engine-loop-heavy-vehicletank
--- scripts/combine.gd
+++ scripts/combine.gd
@@ -1,7 +1,12 @@
 extends VehicleBody
 
-var max_rpm = 500
+var max_rpm = 500.0
 var max_torque = 2000
+var idle_sound_db = 0.0
+var accelerating_sound_db = 6.0
+
+func _ready():
+	$sound.play()
 
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("right","left") * 0.4, 5 * delta)
@@ -9,10 +14,14 @@ func _physics_process(delta):
 	
 	var left_rpm = $back_left_wheel.get_rpm()
 	$back_left_wheel.engine_force = acceleration * max_torque * (1 - left_rpm / max_rpm)
-	
+
 	var right_rpm = $back_right_wheel.get_rpm()
 	$back_right_wheel.engine_force = acceleration * max_torque * (1 - right_rpm / max_rpm)
-	
+
+	if acceleration != 0:
+		$sound.unit_db = accelerating_sound_db
+	else:
+		$sound.unit_db = idle_sound_db
 
 func _input(_ev):
 	if Input.is_action_just_pressed("reset"):
