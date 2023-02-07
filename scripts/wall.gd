7a8f0c5487b8843529077595e47d4e20779348c7
Thomas Holmes <thomasholmes@outlook.com>
2023-02-07 23:03:29 +0000

    Walls now create extra models without stretching, as they should've done in the first place.

--- scripts/wall.gd
+++ scripts/wall.gd
@@ -2,12 +2,31 @@ tool
 
 extends Spatial
 
-export (int, 1, 1000) var width = 1
+export (int, 1, 1000, 1) var length = 1
+var current_length = 0
+
+var wall_chunk_template;
+
+var padding = 1.0
+
 
 func _enter_tree():
-	$MeshInstance.scale.z = width
-	pass
+	wall_chunk_template = preload("res://wall_chunk.tscn")
+	create_wall_chunks()
 
-func _process(_delta):
-	if Engine.editor_hint:
-		$MeshInstance.scale.z = width
+# Called when the node enters the scene tree for the first time.
+func _process(delta):
+	if Engine.editor_hint && length != current_length:
+		create_wall_chunks()
+		current_length = length
+
+func create_wall_chunks():
+	for node in get_children():
+		node.queue_free()
+	# multiply by -0.5 to center wall chunks around node origin
+	var z = -0.5 * padding * length
+	for _n in length:
+		var instance = wall_chunk_template.instance()
+		instance.translation.z = z
+		add_child(instance)
+		z += padding
