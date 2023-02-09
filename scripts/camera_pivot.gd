4cd749b7ef76d6d02f9f4e07c8fcb7bf1f0da5a6
Thomas Holmes <thomasholmes@outlook.com>
2023-02-09 23:41:00 +0000

    Camera no longer looks backwards when reversing. It sort of works.

--- scripts/camera_pivot.gd
+++ scripts/camera_pivot.gd
@@ -8,7 +8,9 @@ func _physics_process(delta):
 	current_velocity.y = 0
 	if current_velocity.length_squared() > 1:
 		direction = lerp(direction, -current_velocity.normalized(), smooth_speed*delta)
-	global_transform.basis = get_rotation_from_direction(direction)
+	
+	if current_velocity.length() > 2.5 && !get_parent().is_reversing():
+		global_transform.basis = get_rotation_from_direction(direction)
 
 func get_rotation_from_direction(look_direction: Vector3) -> Basis:
 	look_direction = look_direction.normalized()
--- scripts/combine.gd
+++ scripts/combine.gd
@@ -11,7 +11,7 @@ func _ready():
 func _physics_process(delta):
 	steering = lerp(steering, Input.get_axis("right","left") * -0.4, 5 * delta)
 	var acceleration = Input.get_axis("back","forward")
-	
+
 	var left_rpm = $back_left_wheel.get_rpm()
 	$back_left_wheel.engine_force = acceleration * max_torque * (1 - left_rpm / max_rpm)
 
@@ -28,3 +28,6 @@ func _physics_process(delta):
 func _input(_ev):
 	if Input.is_action_just_pressed("reset"):
 		rotation = Vector3.UP
+
+func is_reversing():
+	return $back_left_wheel.get_rpm() < 0 && $back_right_wheel.get_rpm() < 0
