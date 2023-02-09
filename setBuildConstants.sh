f15563f857c58d9d7bf524d021cbd305d22b8d26
Thomas Holmes <thomasholmes@outlook.com>
2023-02-09 21:57:54 +0000

    Maybe fixed git permission problem.

--- setBuildConstants.sh
+++ setBuildConstants.sh
@@ -4,6 +4,9 @@
 COMMIT_HASH=$(git rev-parse --short HEAD)
 BUILD_DATE=$(date)
 
+echo "build hash: $COMMIT_HASH"
+git show
+
 sed -i "s/var commit_hash = \"dev\"/var commit_hash = \"$COMMIT_HASH\"/" singletons/build_info.gd
 sed -i "s/var build_time = \"dev\"/var build_time = \"$BUILD_DATE\"/" singletons/build_info.gd
 
