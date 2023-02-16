f9b0ceafbb64b7fe1cc34a6bdc7dd3e1ba52834f
Thomas Holmes <thomasholmes@outlook.com>
2023-02-16 22:42:25 +0000

    Chase cam now follows closer. Increased fov.

--- scripts/camera_pivot.gd
+++ scripts/camera_pivot.gd
@@ -1,15 +1,15 @@
 extends Spatial
 
 var direction = Vector3.FORWARD
-export (float,1,10,0.1) var smooth_speed = 2.5
+export (float,1,10,0.1) var smooth_speed = 7.0
 
 func _physics_process(delta):
 	var current_velocity = get_parent().get_linear_velocity()
 	current_velocity.y = 0
-	if current_velocity.length_squared() > 1:
+	if current_velocity.length_squared() > 1.0:
 		direction = lerp(direction, -current_velocity.normalized(), smooth_speed*delta)
 	
-	if Global.camera_reverses || ( current_velocity.length() > 2.5 && !get_parent().is_reversing()):
+	if Global.camera_reverses || ( current_velocity.length() > 0.5 && !get_parent().is_reversing()):
 		global_transform.basis = get_rotation_from_direction(direction)
 
 func get_rotation_from_direction(look_direction: Vector3) -> Basis:
