686fecd70f3b13962bc4a2e622a4aaaceaf6925b
Thomas Holmes <thomasholmes@outlook.com>
2023-02-19 14:28:33 +0000

    Added FPS counter.

--- singletons/build_info.gd
+++ singletons/build_info.gd
@@ -4,4 +4,8 @@ var commit_hash = "dev"
 var build_time = "dev"
 
 func _ready():
-	$gui/RichTextLabel.text = "Build hash: %s | Build date: %s" % [ commit_hash, build_time ]
+	$"%build_label".text = "Build hash: %s | Build date: %s" % [ commit_hash, build_time ]
+
+
+func _process(_delta: float) -> void:
+	$"%fps_label".text = "FPS: %s" % Engine.get_frames_per_second()
