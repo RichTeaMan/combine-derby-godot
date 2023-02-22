68de0a1db1b1ac250fb97d6c39f92fab10495ddc
Thomas Holmes <thomasholmes@outlook.com>
2023-02-22 22:58:06 +0000

    Added chickens.

--- obstacles/chicken.gd
+++ obstacles/chicken.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-func cow_body_entered(body: Node):
+func chicken_body_entered(body: Node):
 	if !$"%sound".is_playing() && Global.is_in_vehicle_group(body):
 		$"%sound".play()
-		Global.add_points(body.player_id, 5, "Bull bounces")
+		Global.add_points(body.player_id, 1, "Chickens")
