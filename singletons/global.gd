f8ff7bfaf5ade180e8f534878db4be965bace9ca
Thomas Holmes <thomasholmes@outlook.com>
2023-02-25 20:45:13 +0000

    Added combine refresh.

--- singletons/global.gd
+++ singletons/global.gd
@@ -173,7 +173,7 @@ func create_game(player_count: int, game_mode: String, arena_name: String):
 	var game_instance = game_type.instance()
 	game_instance.player_count = player_count
 
-	var combine_template = preload("res://vehicles/combine.tscn")
+	var combine_template = preload("res://vehicles/combine_2.tscn")
 	for i in player_count:
 		var player_id = i + 1
 		var combine_instance = combine_template.instance()
