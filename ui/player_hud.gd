8e35456e6250f36d6780d2b1ceebb5fdcd43b90c
Thomas Holmes <thomasholmes@outlook.com>
2023-02-26 03:38:54 +0000

    Added control hint.

--- ui/player_hud.gd
+++ ui/player_hud.gd
@@ -14,6 +14,14 @@ func _ready():
 	_on_points(player_id, 0, "")
 	_on_speed(player_id, 0)
 
+	var template = "[center]Control with %s keys.[/center]"
+	var control_text = ""
+	if player_id == 1:
+		control_text = template % "WASD"
+	elif player_id == 2:
+		control_text = template % "IJKL"
+	$"%control_label".bbcode_text = control_text
+
 func _on_player_ui(player_id: int, ui: Node) -> void:
 	print("something rec")
 	print("%s -> %s" % [self.player_id, player_id])
@@ -26,9 +34,13 @@ func _on_points(signal_player_id, score, _category):
 	if signal_player_id != player_id || !game_active:
 		return
 	current_points += score
-	$gui/points_label.text = "Points: %s" % current_points
+	$gui/points_label.text = "    Points: %s" % current_points
 
 func _on_speed(signal_player_id, speed):
 	if signal_player_id != self.player_id:
 		return
 	$gui2/speed_label.text = "Speed: %.1fm/s" % speed
+
+
+func _on_Timer_timeout():
+	$AnimationPlayer.play("control_fade")
