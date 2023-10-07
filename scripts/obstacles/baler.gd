67ad863ae222522135e846af939ccec84fc4830a
Thomas Holmes <thomasholmes@outlook.com>
2023-10-07 12:41:43 +0100

    Added baler as a seperate scene. It now has an invisble hay basket.

--- scripts/obstacles/baler.gd
+++ scripts/obstacles/baler.gd
@@ -6,6 +6,7 @@ var stored_wheat = 0
 
 var bale_rotation = PI * 0.5
 var bale_height = 1.20
+var spawn_height = 16
 
 @onready var bale_template = preload("res://obstacles/hay_bale.tscn")
 
@@ -24,7 +25,7 @@ func deposit_wheat(wheat: int) -> void:
 		bales_to_spawn += 1
 		stored_wheat -= BALE_LIMIT
 	print("Created %s bales" % bales_to_spawn)
-	var spawn_point = position + Vector3(0, 5 + bale_height, 0)
+	var spawn_point = position + Vector3(0, spawn_height + bale_height, 0)
 	for i in bales_to_spawn:
 		var bale_instance = bale_template.instantiate()
 		#bale_instance.rotate_z(bale_rotation)
