2b3dc3d5a3061d6a8a10bb71fe4da6c16ea0c33a
Thomas Holmes <thomasholmes@outlook.com>
2023-12-25 20:21:08 +0000

    Added basic gizmo and 3d rotation.

---
+++ editor/editor.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+var rotating = false
+var rotation_constant = 0.5
+var prev_mouse_position
+var next_mouse_position
+
+func _process(delta):
+	if (Input.is_action_just_pressed("rotate")):
+		rotating = true
+		prev_mouse_position = get_viewport().get_mouse_position()
+	if (Input.is_action_just_released("rotate")):
+		rotating = false
+		
+	if (rotating):
+		next_mouse_position = get_viewport().get_mouse_position()
+		%gimbal.rotate_y((next_mouse_position.x - prev_mouse_position.x) * rotation_constant * delta)
+		%gimbal.rotate_x((next_mouse_position.y - prev_mouse_position.y) * rotation_constant * delta)
+		prev_mouse_position = next_mouse_position
+	
