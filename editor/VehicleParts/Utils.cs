87f6cafd320fe53cae4b754535ab20172df8a687
Thomas Holmes <thomasholmes@outlook.com>
2024-02-11 18:49:08 +0000

    Render screenshots now have background transparency.

--- editor/VehicleParts/Utils.cs
+++ editor/VehicleParts/Utils.cs
@@ -15,4 +15,52 @@ public static class Utils
         }
         return children;
     }
+
+    /// <summary>
+    /// Hides all visible descendant nodes, returning ones that were changed.
+    /// </summary>
+    /// <returns></returns>
+    public static Node[] HideVisibleNodeChildrenRecursive(this Node node)
+    {
+        var result = new List<Node>();
+
+        foreach (var n in node.ChildrenRecursive())
+        {
+            if (n is Node3D node3d)
+            {
+                node3d.Visible = false;
+                result.Add(n);
+            }
+            if (n is Node2D node2d)
+            {
+                node2d.Visible = false;
+                result.Add(n);
+            }
+            if (n is Control control)
+            {
+                control.Visible = false;
+                result.Add(n);
+            }
+        }
+        return result.ToArray();
+    }
+
+    public static void RestoreVisibleNodes(this Node[] nodes)
+    {
+        foreach (var n in nodes)
+        {
+            if (n is Node3D node3d)
+            {
+                node3d.Visible = true;
+            }
+            if (n is Node2D node2d)
+            {
+                node2d.Visible = true;
+            }
+            if (n is Control control)
+            {
+                control.Visible = true;
+            }
+        }
+    }
 }
--- tools/scripts/RenderStudio.cs
+++ tools/scripts/RenderStudio.cs
@@ -1,17 +1,27 @@
 using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 using Godot;
 
 public partial class RenderStudio : Node3D
 {
+    readonly static Color MAGIC_PINK = new(1.0f, 0.0f, 1.0f);
+
+    readonly static Color TRANSPARENT = new(0.0f, 0.0f, 0.0f, 0.0f);
+
     private bool rotating = false;
     private float rotation_constant = 0.5f;
     private float zoom_constant = 0.2f;
     private Vector2 prevMousePosition;
     private Vector2 nextMousePosition;
     private Node3D Container => GetNode<Node3D>("%container");
+    private Camera3D Camera => GetNode<Camera3D>("%camera");
+    private Label StatusLabel => GetNode<Label>("%label_status");
 
-    private Node3D Camera => GetNode<Camera3D>("%camera");
+    private int framesToScreenshot = -1;
+
+    private Node[] previousHiddenNodes = Array.Empty<Node>();
 
     public void LoadFromResPath(string resource)
     {
@@ -28,6 +38,41 @@ public partial class RenderStudio : Node3D
 
     public override void _Process(double delta)
     {
+        if (framesToScreenshot > 0)
+        {
+            framesToScreenshot--;
+            return;
+        }
+        if (framesToScreenshot == 0)
+        {
+            framesToScreenshot = -1;
+
+            string filepath = $"screenshot-{DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture)}.png";
+            using var screenShot = GetViewport().GetTexture().GetImage();
+            using var transparentScreenshot = Image.Create(screenShot.GetWidth(), screenShot.GetHeight(), false, Image.Format.Rgba8);
+            foreach (var x in Enumerable.Range(0, screenShot.GetWidth()))
+            {
+                foreach (var y in Enumerable.Range(0, screenShot.GetHeight()))
+                {
+                    var pixel = screenShot.GetPixel(x, y);
+                    if (pixel == MAGIC_PINK)
+                    {
+                        transparentScreenshot.SetPixel(x, y, TRANSPARENT);
+                    }
+                    else
+                    {
+                        transparentScreenshot.SetPixel(x, y, pixel);
+                    }
+                }
+            }
+            transparentScreenshot.SavePng(filepath);
+            
+            StatusLabel.Text = $"Screenshot saved to {filepath}.";
+            GD.Print($"Screenshot saved to {filepath}.");
+
+            previousHiddenNodes.RestoreVisibleNodes();
+        }
+
         if (rotating)
         {
             nextMousePosition = GetViewport().GetMousePosition();
@@ -49,6 +94,10 @@ public partial class RenderStudio : Node3D
 
     public override void _UnhandledInput(InputEvent _inputEvent)
     {
+        if (framesToScreenshot > 0)
+        {
+            return;
+        }
         if (Input.IsActionJustPressed("rotate"))
         {
             rotating = true;
@@ -60,10 +109,20 @@ public partial class RenderStudio : Node3D
         }
         if (Input.IsActionJustPressed("screenshot"))
         {
-            string filepath = $"screenshot-{DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture)}.png";
-            var screenShot = GetViewport().GetTexture().GetImage();
-            screenShot.SavePng(filepath);
-            GD.Print($"Screenshot save to {filepath}.");
+            previousHiddenNodes = GetTree().Root.HideVisibleNodeChildrenRecursive();
+            Container.Visible = true;
+            Container.ChildrenRecursive().ToArray().RestoreVisibleNodes();
+            List<Node> parents = new List<Node>();
+            var parent = Container.GetParent();
+            while (parent != null)
+            {
+                parents.Add(parent);
+                parent = parent.GetParent();
+            }
+            parents.ToArray().RestoreVisibleNodes();
+
+            // it will take at least a frame for visibilty changes to render, so set a timer to do it later.
+            framesToScreenshot = 1;
         }
 
     }
