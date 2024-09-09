1a84ec3b61a869dc135cf30c774a0aea4e901bd7
Thomas Holmes <thomasholmes@outlook.com>
2024-09-09 15:44:41 +0100

    Game type can now be selected.

--- ui/GameSelectScreen.cs
+++ ui/GameSelectScreen.cs
@@ -3,7 +3,12 @@ using System;
 
 public partial class GameSelectScreen : Control
 {
+    private CheckButton buttonArena => GetNode<CheckButton>("%button_arena");
+
+    private CheckButton buttonHarvest => GetNode<CheckButton>("%button_harvest");
+
     private VehicleSelect vehicleSelect1 => GetNode<VehicleSelect>("%vehicle_select_1");
+
     private VehicleSelect vehicleSelect2 => GetNode<VehicleSelect>("%vehicle_select_2");
 
     private int readyPlayers = 0;
@@ -16,6 +21,21 @@ public partial class GameSelectScreen : Control
     {
         vehicleSelect1.PlayRequested += onVehicleSelected;
         vehicleSelect2.PlayRequested += onVehicleSelected;
+
+        buttonArena.ButtonPressed = true;
+        buttonHarvest.ButtonPressed = false;
+
+        buttonArena.Pressed += () => { gameTypePressed(buttonArena); };
+        buttonHarvest.Pressed += () => { gameTypePressed(buttonHarvest); };
+    }
+
+    private void gameTypePressed(CheckButton button)
+    {
+
+        buttonArena.ButtonPressed = false;
+        buttonHarvest.ButtonPressed = false;
+
+        button.ButtonPressed = true;
     }
 
     private void onVehicleSelected(VehicleSelect vehicleSelect)
@@ -28,8 +48,34 @@ public partial class GameSelectScreen : Control
                 new Player(1, vehicleSelect1.SelectedVehicleName),
                 new Player(2, vehicleSelect2.SelectedVehicleName)
             };
-            GlobalCs.Current.CreateGame(players, GlobalCs.MODE_POINTS, GlobalCs.ARENA_CRASH);
+            GlobalCs.Current.CreateGame(players, FetchCheckedGameMode(), FetchCheckedArenaName());
             GD.Print("Game created");
         }
+    }
+
+    private string FetchCheckedArenaName()
+    {
+        if (buttonArena.ButtonPressed)
+        {
+            return GlobalCs.ARENA_CRASH;
+        }
+        if (buttonHarvest.ButtonPressed)
+        {
+            return GlobalCs.ARENA_FARM;
+        }
+        return GlobalCs.ARENA_CRASH;
+    }
+
+        private string FetchCheckedGameMode()
+    {
+        if (buttonArena.ButtonPressed)
+        {
+            return GlobalCs.MODE_POINTS;
+        }
+        if (buttonHarvest.ButtonPressed)
+        {
+            return GlobalCs.MODE_HARVEST;
+        }
+        return GlobalCs.MODE_POINTS;
     }
 }
